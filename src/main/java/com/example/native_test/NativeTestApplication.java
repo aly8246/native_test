9f4f9cfa4df831078a2e1cc49995c6904d28f8c2
aly8246 <1558146696@qq.com>
2022-07-16 05:24:09 +0800

    add

--- src/main/java/com/example/native_test/NativeTestApplication.java
+++ src/main/java/com/example/native_test/NativeTestApplication.java
@@ -1,13 +1,15 @@
 package com.example.native_test;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.native_test.mapper")
 public class NativeTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NativeTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NativeTestApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/native_test/controller/UserController.java
+++ src/main/java/com/example/native_test/controller/UserController.java
@@ -1,2 +1,28 @@
-package com.example.native_test.controller;public class UserController {
+package com.example.native_test.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.native_test.User;
+import com.example.native_test.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("user")
+public class UserController {
+    private final UserMapper userMapper;
+
+    @GetMapping
+    public Object userList() {
+        Page<User> p = new Page<>(1, 20);
+        return userMapper.selectPage(p,
+                new LambdaQueryWrapper<>()
+        );
+    }
+
+
 }
