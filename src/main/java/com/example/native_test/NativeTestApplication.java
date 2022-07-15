c1a951411d14c34ca5c9b2e39f66109cd7e6fe5e
aly8246 <1558146696@qq.com>
2022-07-16 05:02:34 +0800

    Initial commit

---
+++ src/main/java/com/example/native_test/NativeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.native_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/native_test/controller/UserController.java
@@ -0,0 +1,2 @@
+package com.example.native_test.controller;public class UserController {
+}
